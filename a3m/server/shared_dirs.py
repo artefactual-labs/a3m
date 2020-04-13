1c93c9658a14394bd4d68f86c747d7148222cdb8
Jesús García Crespo <jesus@sevein.com>
2020-04-12 20:07:11 -0700

    Remove send to backlog workflow

--- a3m/server/shared_dirs.py
+++ a3m/server/shared_dirs.py
@@ -82,11 +82,6 @@ DEFAULT_PROCESSING_CONFIG = """<processingMCP>
 
 AUTOMATED_PROCESSING_CONFIG = """<processingMCP>
   <preconfiguredChoices>
-    <!-- Store DIP -->
-    <preconfiguredChoice>
-      <appliesTo>5e58066d-e113-4383-b20b-f301ed4d751c</appliesTo>
-      <goToChain>8d29eb3d-a8a8-4347-806e-3d8227ed44a1</goToChain>
-    </preconfiguredChoice>
     <!-- Select compression level -->
     <preconfiguredChoice>
       <appliesTo>01c651cb-c174-4ba4-b985-1d87a44d6754</appliesTo>
@@ -137,11 +132,6 @@ AUTOMATED_PROCESSING_CONFIG = """<processingMCP>
       <appliesTo>f09847c2-ee51-429a-9478-a860477f6b8d</appliesTo>
       <goToChain>d97297c7-2b49-4cfe-8c9f-0613d63ed763</goToChain>
     </preconfiguredChoice>
-    <!-- Store DIP location -->
-    <preconfiguredChoice>
-      <appliesTo>cd844b6e-ab3c-4bc6-b34f-7103f88715de</appliesTo>
-      <goToChain>/api/v2/location/default/DS/</goToChain>
-    </preconfiguredChoice>
     <!-- Generate transfer structure report -->
     <preconfiguredChoice>
       <appliesTo>56eebd45-5600-4768-a8c2-ec0114555a3d</appliesTo>
@@ -167,11 +157,6 @@ AUTOMATED_PROCESSING_CONFIG = """<processingMCP>
       <appliesTo>01d64f58-8295-4b7b-9cab-8f1b153a504f</appliesTo>
       <goToChain>9475447c-9889-430c-9477-6287a9574c5b</goToChain>
     </preconfiguredChoice>
-    <!-- Store AIP -->
-    <preconfiguredChoice>
-      <appliesTo>2d32235c-02d4-4686-88a6-96f4d6c7b1c3</appliesTo>
-      <goToChain>9efab23c-31dc-4cbd-a39d-bb1665460cbe</goToChain>
-    </preconfiguredChoice>
     <!-- Perform policy checks on access derivatives -->
     <preconfiguredChoice>
       <appliesTo>8ce07e94-6130-4987-96f0-2399ad45c5c2</appliesTo>
@@ -192,11 +177,6 @@ AUTOMATED_PROCESSING_CONFIG = """<processingMCP>
       <appliesTo>bd899573-694e-4d33-8c9b-df0af802437d</appliesTo>
       <goToChain>2dc3f487-e4b0-4e07-a4b3-6216ed24ca14</goToChain>
     </preconfiguredChoice>
-    <!-- Store AIP location -->
-    <preconfiguredChoice>
-      <appliesTo>b320ce81-9982-408a-9502-097d0daa48fa</appliesTo>
-      <goToChain>/api/v2/location/default/AS/</goToChain>
-    </preconfiguredChoice>
     <!-- Document empty directories -->
     <preconfiguredChoice>
       <appliesTo>d0dfa5fc-e3c2-4638-9eda-f96eea1070e0</appliesTo>
@@ -211,11 +191,6 @@ AUTOMATED_PROCESSING_CONFIG = """<processingMCP>
     <preconfiguredChoice>
       <appliesTo>de909a42-c5b5-46e1-9985-c031b50e9d30</appliesTo>
       <goToChain>1e0df175-d56d-450d-8bee-7df1dc7ae815</goToChain>
-    </preconfiguredChoice>
-    <!-- Upload DIP -->
-    <preconfiguredChoice>
-      <appliesTo>92879a29-45bf-4f0b-ac43-e64474f0f2f9</appliesTo>
-      <goToChain>6eb8ebe7-fab3-4e4c-b9d7-14de17625baa</goToChain>
     </preconfiguredChoice>
   </preconfiguredChoices>
 </processingMCP>
@@ -294,9 +269,6 @@ def create():
         "watchedDirectories/workFlowDecisions/selectFormatIDToolTransfer",
         "www",
         "www/AIPsStore",
-        "www/AIPsStore/transferBacklog",
-        "www/AIPsStore/transferBacklog/arrange",
-        "www/AIPsStore/transferBacklog/originals",
         "www/DIPsStore",
     )
     for dirname in dirs:
