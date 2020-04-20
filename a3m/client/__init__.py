2a15fb4c8bdf45cc987676d87d7d9ed661acf37c
Douglas Cerna (Soy Douglas) <douglascerna@yahoo.com>
2020-04-19 18:57:03 -0600

    Add trailing slash to ASSETS_DIR

--- a3m/client/__init__.py
+++ a3m/client/__init__.py
@@ -5,5 +5,5 @@ import os
 
 THIS_DIR = os.path.dirname(os.path.abspath(__file__))
 
-ASSETS_DIR = os.path.join(THIS_DIR, "assets")
+ASSETS_DIR = os.path.join(THIS_DIR, "assets", "")
 MODULES_FILE = os.path.join(ASSETS_DIR, "modules.ini")
