ed2847e77d5628c0d8d55142ed2ef8f1a1559a67
Jesús García Crespo <jesus@sevein.com>
2020-04-26 17:42:02 -0700

    Remove unused workflow paths

--- a3m/client/clientScripts/failed_transfer_cleanup.py
+++ a3m/client/clientScripts/failed_transfer_cleanup.py
@@ -3,38 +3,25 @@ import argparse
 from django.db import transaction
 
 from a3m.client import metrics
-from a3m.main.models import File
 from a3m.main.models import Transfer
 
 
-REJECTED = "reject"
 FAILED = "fail"
 
 
-def main(job, fail_type, transfer_uuid, transfer_path):
-    # Delete files for reingest transfer
-    # A new reingest doesn't know to delete this because the UUID is different from the AIP, and it causes problems when re-parsing these files
+def main(job, transfer_uuid, transfer_path):
     transfer = Transfer.objects.get(uuid=transfer_uuid)
-    if transfer.type == "Archivematica AIP":
-        File.objects.filter(transfer_id=transfer_uuid).delete()
-
-    metrics.transfer_failed(transfer.type, fail_type)
+    metrics.transfer_failed(transfer.type, FAILED)
 
     return 0
 
 
 def call(jobs):
-    parser = argparse.ArgumentParser(
-        description="Cleanup from failed/rejected Transfers."
-    )
-    parser.add_argument("fail_type", help=f'"{REJECTED}" or "{FAILED}"')
+    parser = argparse.ArgumentParser(description="Cleanup from failed Transfers.")
     parser.add_argument("transfer_uuid", help="%SIPUUID%")
-    parser.add_argument("transfer_path", help="%SIPDirectory%")
 
     with transaction.atomic():
         for job in jobs:
             with job.JobContext():
                 args = parser.parse_args(job.args[1:])
-                job.set_status(
-                    main(job, args.fail_type, args.transfer_uuid, args.transfer_path)
-                )
+                job.set_status(main(job, args.transfer_uuid))
