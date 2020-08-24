372fcdcc4389d82751c1fa5613af48343e373b26
Jesús García Crespo <jesus@sevein.com>
2020-08-24 08:34:59 +0200

    Enable WAL mode

---
+++ a3m/main/__init__.py
@@ -0,0 +1,10 @@
+from django.db.backends.signals import connection_created
+
+
+def activate_wal_mode(sender, connection, **kwargs):
+    if connection.vendor == "sqlite":
+        cursor = connection.cursor()
+        cursor.execute("PRAGMA journal_mode=WAL;")
+
+
+connection_created.connect(activate_wal_mode)
