5e65f56e1aea487e4ff4eaa0b9648fad54e9c453
José Raddaoui Marín <raddaouimarin@gmail.com>
2023-05-18 21:46:46 +0200

    Optimize transaction in checksums and sizes job
    Adapted from https://github.com/artefactual/archivematica/pull/1820.

--- a3m/client/clientScripts/update_size_and_checksum.py
+++ a3m/client/clientScripts/update_size_and_checksum.py
@@ -21,7 +21,7 @@ import uuid
 
 from django.db import transaction
 
-from a3m.archivematicaFunctions import chunk_iterable
+from a3m.fileOperations import get_size_and_checksum
 from a3m.fileOperations import updateSizeAndChecksum
 from a3m.main.models import File
 
@@ -58,20 +58,8 @@ def get_sip_file_queryset(sip_uuid, filter_subdir):
     return files
 
 
-def update_size_and_checksum_for_file(
-    job,
-    file_,
-    sip_directory,
-    transfer_uuid,
-    date,
-    event_uuid,
-):
-    """Update size and checksum for file in database.
-
-    If file is from Archivematica AIP transfer, try to extract and use
-    the size, checksum, and checksum type values from the METS.
-    """
-    kw = {}
+def get_size_and_checksum_for_file(file_, sip_directory, transfer_uuid):
+    """Get size and checksum for a file."""
     if transfer_uuid:
         file_path = file_.currentlocation.replace(
             TRANSFER_REPLACEMENT_PATH_STRING, sip_directory
@@ -82,40 +70,16 @@ def update_size_and_checksum_for_file(
         )
 
     if not os.path.exists(file_path):
-        return
+        return {}
 
-    job.print_output(f"Updating file size and checksum for file {file_.uuid}")
-    updateSizeAndChecksum(file_.uuid, file_path, date, event_uuid, **kw)
+    fileSize, checksum, checksumType = get_size_and_checksum(file_path)
 
-
-def update_files(
-    job,
-    files,
-    sip_directory,
-    transfer_uuid,
-    date,
-    event_uuid,
-):
-    """Update file sizes and checksums for each file in list.
-
-    We open a database transaction for each chunk of 10 files, in an
-    attempt to balance performance with reasonable transaction lengths.
-    """
-    for file_chunk in chunk_iterable(files):
-        with transaction.atomic():
-            for file_ in file_chunk:
-                if not file_:
-                    continue
-                update_size_and_checksum_for_file(
-                    job,
-                    file_,
-                    sip_directory,
-                    transfer_uuid,
-                    date,
-                    event_uuid,
-                )
-
-    return 0
+    return {
+        "filePath": file_path,
+        "fileSize": fileSize,
+        "checksum": checksum,
+        "checksumType": checksumType,
+    }
 
 
 def call(jobs):
@@ -136,6 +100,8 @@ def call(jobs):
         dest="event_uuid",
     )
 
+    state = []
+
     for job in jobs:
         with job.JobContext(logger=logger):
             args = parser.parse_args(job.args[1:])
@@ -144,19 +110,26 @@ def call(jobs):
             if all(TRANSFER_SIP_UUIDS) or not any(TRANSFER_SIP_UUIDS):
                 job.print_error("SIP exclusive-or Transfer UUID must be defined")
                 job.set_status(2)
-                return
+                continue
 
             files = get_transfer_file_queryset(args.transfer_uuid, args.filter_subdir)
             if args.sip_uuid:
                 files = get_sip_file_queryset(args.sip_uuid, args.filter_subdir)
 
-            job.set_status(
-                update_files(
-                    job,
-                    files,
-                    args.sip_directory,
-                    args.transfer_uuid,
-                    args.date,
-                    args.event_uuid,
+            for file_ in files:
+                if not file_:
+                    continue
+                file_info = get_size_and_checksum_for_file(
+                    file_, args.sip_directory, args.transfer_uuid
                 )
+                if file_info:
+                    state.append((file_.uuid, file_info, args))
+
+            job.set_status(0)
+
+    with transaction.atomic():
+        for file_uuid, file_info, args in state:
+            file_path = file_info.pop("filePath")
+            updateSizeAndChecksum(
+                file_uuid, file_path, args.date, args.event_uuid, **file_info
             )
