235d789066681aaccde8ef2633748e53cb86d1a0
Jesús García Crespo <jesus@sevein.com>
2023-05-30 10:34:00 +0000

    Remove unneeded semicolon

--- a3m/main/__init__.py
+++ a3m/main/__init__.py
@@ -4,7 +4,7 @@ from django.db.backends.signals import connection_created
 def activate_wal_mode(sender, connection, **kwargs):
     if connection.vendor == "sqlite":
         cursor = connection.cursor()
-        cursor.execute("PRAGMA journal_mode=WAL;")
+        cursor.execute("PRAGMA journal_mode=WAL")
 
 
 connection_created.connect(activate_wal_mode)
