984ba1bed8f4aa54d6d5db341e070ebc0f07b5ea
Jesús García Crespo <jesus@sevein.com>
2023-09-19 08:42:56 +0000

    Use local XML schemas for XML validation

--- a3m/client/__init__.py
+++ a3m/client/__init__.py
@@ -1,6 +1,12 @@
 import os
+import pathlib
 
 
 THIS_DIR = os.path.dirname(__file__)
 
 ASSETS_DIR = os.path.join(THIS_DIR, "assets", "")
+
+# Use local XML schemas for validation.
+os.environ["XML_CATALOG_FILES"] = str(
+    pathlib.Path(__file__).parent / "assets/catalog/catalog.xml"
+)
